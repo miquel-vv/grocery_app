14283584e986f15346324abf398484a3b3a8976c
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2021-12-12 11:36:09 +0100

    Add preferences for token storage.

--- app/src/main/java/com/example/mealplanner/data/LoginDataSource.kt
+++ app/src/main/java/com/example/mealplanner/data/LoginDataSource.kt
@@ -1,11 +1,25 @@
 package com.example.mealplanner.data
 
-import java.io.IOException
+import android.content.Context
+import org.json.JSONException
 
-class LoginDataSource {
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.preference.PreferenceManager
 
-    fun login(email: String, password: String){
 
+class LoginDataSource(private val context: Context) {
+
+    fun storeToken(token:String){
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val edit: SharedPreferences.Editor = prefs.edit()
+        try {
+            edit.putString("token", token)
+            Log.i("Login", token)
+            edit.apply()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
     }
 
     fun logout() {
