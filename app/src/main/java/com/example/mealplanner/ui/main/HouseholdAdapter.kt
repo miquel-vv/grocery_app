115f576a739634c097538c055464f66d32bf47ac
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2022-04-16 13:00:04 +0200

    Update household list item.

--- app/src/main/java/com/example/mealplanner/ui/main/HouseholdAdapter.kt
+++ app/src/main/java/com/example/mealplanner/ui/main/HouseholdAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.mealplanner.ui.main
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +10,7 @@ import com.example.mealplanner.TextItemViewHolder
 import com.example.mealplanner.data.model.Household
 
 
-class HouseholdAdapter:RecyclerView.Adapter<TextItemViewHolder>() {
+class HouseholdAdapter:RecyclerView.Adapter<HouseholdAdapter.ViewHolder>() {
     var data = listOf<Household>()
         set(value){
             field = value
@@ -18,14 +19,21 @@ class HouseholdAdapter:RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.name;
+        holder.name.text = item.name;
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.household_list_item, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.household_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val name: TextView = itemView.findViewById(R.id.household_name)
+        val memberStatus : TextView = itemView.findViewById(R.id.member_status)
     }
 }
+
+
