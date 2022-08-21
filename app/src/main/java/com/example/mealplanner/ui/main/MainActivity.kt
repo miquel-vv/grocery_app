5c7a58a363dd618e2f6a42edfe8ea63a9ca39e78
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2022-08-21 21:31:06 +0200

    Add log out functionality.

--- app/src/main/java/com/example/mealplanner/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mealplanner/ui/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.mealplanner.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.mealplanner.R
+import com.example.mealplanner.data.repo.LoginRepository
 import com.example.mealplanner.databinding.ActivityMainBinding
 
 
@@ -15,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private val loginRepo = LoginRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    fun onLogOut(item: MenuItem) {
+        Log.d("MAIN_ACTIVITY", "onLogOut: logging out...")
+        LoginRepository.logout()
+        val intent = Intent(this, com.example.mealplanner.ui.landing.MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
